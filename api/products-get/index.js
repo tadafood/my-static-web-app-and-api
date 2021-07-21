c761b5b418bf6ce8c033940873a811b88f10272f
kawashima <t_kawashima@webnext.co.jp>
2021-07-21 18:41:13 +0900

    api変更

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
   context.log("product-get called");
 
   try {
-    const products = data.getProducts;
+    const products = data.getProducts();
     context.res.status(200).json(products);
   } catch (error) {
     context.res.status(500).send(error);
